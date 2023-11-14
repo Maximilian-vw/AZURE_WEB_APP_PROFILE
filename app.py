b35227715861de98c4cb27363db78dcc58b9f1fe
Maximilian <114843063+Maximilian-vw@users.noreply.github.com>
2023-11-14 23:11:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('home.html')
+
+@app.route('/cv')
+def cv():
+    return render_template('cv.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
